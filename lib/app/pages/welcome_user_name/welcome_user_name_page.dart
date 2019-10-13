dbab9b5bd0142a4c32b36692ea095a8c7cbfbd77
Matheus Torreão <matheus.torreao@avanade.com>
2019-10-13 00:03:52 -0300

    Terminado layout da tela WelcomeUserNamePage

--- lib/app/pages/welcome_user_name/welcome_user_name_page.dart
+++ lib/app/pages/welcome_user_name/welcome_user_name_page.dart
@@ -8,13 +8,52 @@ class WelcomeUserNamePage extends StatefulWidget {
 class _WelcomeUserNamePageState extends State<WelcomeUserNamePage> {
   @override
   Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontSize: 25,
+    );
     return Scaffold(
-      appBar: AppBar(
-        
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 32,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Olá,',
+              style: textStyle,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Como devemos te chamar?',
+              style: textStyle.copyWith(
+                fontSize: 24,
+              ),
+              textAlign: TextAlign.start,
+              softWrap: true,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Nome',
+                hintText: 'Alfredo da Silva',
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Column(
-        children: <Widget>[],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.arrow_forward,
+          color: Colors.white,
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 6,
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
