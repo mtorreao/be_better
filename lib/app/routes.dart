f3f9926fcf5ddefd5a8dc84c5a65df449d48cb7b
Matheus Torreão <matheus.torreao@avanade.com>
2019-10-12 23:08:52 -0300

    Adicionando pagina de splash

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:be_better/app/pages/home/home_module.dart';
-import 'package:be_better/app/pages/login/login_module.dart';
+import 'package:be_better/app/pages/splash/splash_page.dart';
+import 'package:flutter/material.dart';
 
 class AppRoutes {
   Map<String, WidgetBuilder> routeMain() {
     return {
-      '/': (context) => LoginModule(),
+      '/': (context) => SplashPage(),
       '/home': (context) => HomeModule()
     };
   }
