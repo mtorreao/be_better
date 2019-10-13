f635816be98c0d72cab9da731ccf01d1e0d28cae
Matheus Torreão <matheus.torreao@avanade.com>
2019-10-12 23:08:10 -0300

    Mudando a paleta de cores

--- lib/app/shared/styles/theme_style.dart
+++ lib/app/shared/styles/theme_style.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class AppTheme {
   ThemeData themeData() {
     return ThemeData(
+      scaffoldBackgroundColor: const Color(0xFFF3E5F5),
+      // backgroundColor: const Color(0xFFF3E5F5),
+      primaryColor: const Color(0xffBA6BCB),
+      accentColor: const Color(0xffD1C4E9),
       textTheme: _textTheme(),
     );
   }
